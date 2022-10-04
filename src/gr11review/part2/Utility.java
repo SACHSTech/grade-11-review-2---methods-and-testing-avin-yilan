ef679a816d380003597c646d53d760e5ac79787d
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2022-10-03 21:52:19 -0400

    1D array questions added to Avin's branch

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -55,6 +55,7 @@ public class Utility {
         }
 	}
     
+
 	/**
 	 * 
 	 * Given a file, method looks for the line with the longest word and returns that to user
@@ -90,7 +91,38 @@ public class Utility {
 	
 	}
 
-	
 
+	/**
+	 * 
+	 * Given an array, a new array is returned with all values equal to ten removed and everything else shifted to the left
+	 * 
+	 * @param nums
+	 * @return modified array with ten's removed and everything shifted to the left
+	 * 
+	 * @author Avin A.
+	 */
+
+	public static int[] withoutTen (int[] nums) {
+
+		int[] newNums = new int[nums.length];
+		int intPlaceHolderInNew = 0;
+		
+		for (int i = 0; i < nums.length; i ++) {
+			if (nums[i] != 10) {
+				newNums[intPlaceHolderInNew] = nums[i];
+                intPlaceHolderInNew++;
+			}
+		}
+
+        while (intPlaceHolderInNew < nums.length) {
+            newNums[intPlaceHolderInNew] = 0;
+            intPlaceHolderInNew++;
+        }
+
+        return newNums;
+	}
+
+
+	
     
 }
