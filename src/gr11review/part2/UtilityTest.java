ea8fab2cda04ff98edc74cd478771654577d9e19
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2022-10-03 22:42:10 -0400

    xyBalance tests added to test file in Avin's

--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -7,18 +7,48 @@ import gr11review.part2.Utility;
 public class UtilityTest {
 
     @Test
-    public void addTest1() {
+    public void xyBalanceTest1() {
         assertEquals (true, Utility.xyBalance("aaxbby"));
     }
 
     @Test
-    public void addTest2() {
+    public void xyBalanceTest2() {
         assertEquals (false, Utility.xyBalance("aaxbb"));
     }
 
     @Test
-    public void addTest3() {
+    public void xyBalanceTest3() {
         assertEquals (false, Utility.xyBalance("yaaxbb"));
     }
+
+    @Test
+    public void xyBalanceTest4() {
+        assertEquals (true, Utility.xyBalance(""));
+    }
+
+    @Test
+    public void xyBalanceTest5() {
+        assertEquals (false, Utility.xyBalance("ljafghuogyx"));
+    }
+
+    @Test
+    public void xyBalanceTest6() {
+        assertEquals (true, Utility.xyBalance("ayayayyyxy"));
+    }
+
+    @Test
+    public void xyBalanceTest7() {
+        assertEquals (false, Utility.xyBalance("x"));
+    }
+
+    @Test
+    public void xyBalanceTest8() {
+        assertEquals (true, Utility.xyBalance("y"));
+    }
     
+
+    @Test
+    public void xyBalanceTest9() {
+        assertEquals (false, Utility.xyBalance("yx"));
+    }
 }
