c8005e81e2e07387c720098a6033ee9b964b49c6
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2022-10-03 22:12:55 -0400

    all questions added to Avin's

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -60,7 +60,7 @@ public class Utility {
 	 * 
 	 * Given a file, method looks for the line with the longest word and returns that to user
 	 * 
-	 * @param filenametxt
+	 * @param filenametxt file input
 	 * @return longest word that exists in file
 	 * 
 	 * @author Avin A.
@@ -96,7 +96,7 @@ public class Utility {
 	 * 
 	 * Given an array, a new array is returned with all values equal to ten removed and everything else shifted to the left
 	 * 
-	 * @param nums
+	 * @param nums array with tens
 	 * @return modified array with ten's removed and everything shifted to the left
 	 * 
 	 * @author Avin A.
@@ -123,6 +123,55 @@ public class Utility {
 	}
 
 
-	
+	/**
+	 * 
+	 * Given n, an array with the length of n(n+1)/2 is created with the values of 1, 1~2, 1~3, 1~n
+	 * 
+	 * @param n number
+	 * @return created array with n(n+1)/2 elements that are sorted: 1, 1, 2, 1, 2, 3, ..., 1, ..., n
+	 * 
+	 * @author Avin A.
+	 */
+
+	public static int[] seriesUp (int n) {
+		int[] myArray = new int[n * (n + 1) / 2];
+		int intPlaceHolder = 0;
+		
+		for (int i = 1; i <= n; i ++) {
+			for (int j = 1; j <= i; j ++) {
+				myArray[intPlaceHolder] = j;
+				intPlaceHolder++;
+			}
+		}
+		
+		return myArray;
+		
+	}
+
+
+	/**
+	 * 
+	 * Given an array, all the values are reversed
+	 * 
+	 * @param arr initial array
+	 * @return array with values reversed
+	 * 
+	 * @author Avin A.
+	 */
+
+	public static int[][] reverse (int[][] arr) {
+
+		int[][] reverseArray = new int[arr.length][arr[0].length];
+													
+		
+		for (int i = 0; i < arr.length; i ++) {
+			for (int j = 0; j < arr[0].length; j ++) {
+				reverseArray[i][j] = arr[arr.length - 1 - i][arr[0].length - 1 - j];
+			}
+		}
+		
+		return reverseArray;
+		
+	}
     
 }
