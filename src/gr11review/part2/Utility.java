a1f7f1a68285d2f0fdc3e73fab6566b4ec5b4110
yilannnh <yilan.hong23@ycdsbk12.ca>
2022-10-06 22:00:24 -0400

    Added more comments

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -23,9 +23,12 @@ public class Utility {
         int sum = 0;
         String number = "";
 
+        // Iterate through the string
         for (int i = 0; i < str.length(); i++) {
+            // Get the character at index i
             char c = str.charAt(i);
 
+            // Check if it's a digit
             if (Character.isDigit(c)) {
                 // Append the digit to the number
                 number = number + c;
@@ -46,34 +49,64 @@ public class Utility {
 
     }
 
+    /**
+     * Write a method alphaWord(String filenametxt) such that given the name of a
+     * file filenametxt that contains a single word on each line, returns the word
+     * that is alphabetically first.
+     * 
+     * @param filenametxt
+     * @return
+     * @throws Exception
+     */
     public static String alphaWord(String filenametxt) throws Exception {
+        // Create a reader to read the file
         BufferedReader br = new BufferedReader(new FileReader(filenametxt));
         String line;
-        String alpha = "zzzzzzzz";
+        // Initialize alpha with a blank string
+        String alpha = "";
+
+        // Read each line, till the end of the file
         while ((line = br.readLine()) != null) {
-            // process the line.
-            if (alpha.compareTo(line) > 0) {
+            // If alpha is alphabetically behind
+            if (alpha.length() == 0 || alpha.compareTo(line) > 0) {
                 // New alpha!
                 alpha = line;
             }
-            
+
         }
 
+        // Close the reader
         br.close();
 
         return alpha;
     }
 
+    /**
+     * We'll say that an element in an array is "alone" if there are values before
+     * and after it, and those values are different from it. Return a version of the
+     * given array where every instance of the given value which is alone is
+     * replaced by whichever value to its left or right is larger. public int[]
+     * notAlone(int[] nums, int val)
+     * 
+     * @param nums
+     * @param value
+     * @return
+     */
     public static int[] notAlone(int[] nums, int value) {
+        // If there are less than 3 elements, simply return the same array because
+        // there's no before/after
         if (nums.length < 3)
             return nums;
-        
+
+        // Iterate from the 2nd element to the 2nd last element
         for (int i = 1; i < nums.length - 1; i++) {
             int num = nums[i];
+            // If the element matches the specified value
             if (num == value) {
                 int before = nums[i - 1];
                 int after = nums[i + 1];
-                
+
+                // Check if the element is alone
                 if (before != num && after != num) {
                     // Found an alone element, replace it with the larger number
                     if (before > after)
@@ -87,23 +120,29 @@ public class Utility {
         return nums;
     }
 
+    /**
+     * Main method I added just to test the methods
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        // Testing with my main method for now, will replace these with test cases
         System.out.println(sumNumbers("abc123xyz"));
         System.out.println(sumNumbers("aa11b33"));
         System.out.println(sumNumbers("7 11"));
 
         System.out.println(alphaWord("src/gr11review/test2/Review2_3Test_1.txt"));
 
-        int nums[] = {1, 2, 3};
+        int nums[] = { 1, 2, 3 };
         nums = notAlone(nums, 2);
         System.out.println(Arrays.toString(nums));
 
-        int nums2[] = {1, 2, 3, 2, 5, 2};
+        int nums2[] = { 1, 2, 3, 2, 5, 2 };
         nums2 = notAlone(nums2, 2);
         System.out.println(Arrays.toString(nums2));
 
-        int nums3[] = {3, 4};
+        int nums3[] = { 3, 4 };
         nums3 = notAlone(nums3, 3);
-        System.out.println(Arrays.toString(nums3));        
+        System.out.println(Arrays.toString(nums3));
     }
 }
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -2,57 +2,63 @@ package gr11review.part2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.file.FileSystems;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import gr11review.part2.Utility;
 
-public class UtilityTest{
+public class UtilityTest {
     @Test
     public void sumNumbersTest1() {
         assertEquals(123, Utility.sumNumbers("abc123xyz"));
     }
+
     @Test
     public void sumNumbersTest2() {
         assertEquals(44, Utility.sumNumbers("aa11b33"));
     }
+
     @Test
     public void sumNumbersTest3() {
         assertEquals(18, Utility.sumNumbers("7 11"));
     }
 
-   
+    @Test
     public void alphaWordTest1() throws Exception {
-        assertEquals("accompany", Utility.alphaWord("UtilityTest.java"));
+        assertEquals("accompany", Utility.alphaWord("bin/gr11review/test2/Review2_3Test_1.txt"));
     }
-    public void alphaWordTest2() {
 
+    @Test
+    public void alphaWordTest2() throws Exception {
+        assertEquals("x", Utility.alphaWord("bin/gr11review/test2/Review2_3Test_2.txt"));
     }
-    public void alphaWordTest3() {
 
+    @Test
+    public void alphaWordTest3() throws Exception {
+        assertEquals("accompanyx", Utility.alphaWord("bin/gr11review/test2/Review2_3Test_3.txt"));
     }
 
     @Test
     public void notAloneTest1() {
-        int nums[] = {1, 2, 3};
+        int nums[] = { 1, 2, 3 };
         nums = Utility.notAlone(nums, 2);
-        assertTrue(Arrays.equals(new int[] {1, 3, 3}, nums));
+        assertTrue(Arrays.equals(new int[] { 1, 3, 3 }, nums));
     }
 
     @Test
     public void notAloneTest2() {
-        int nums2[] = {1, 2, 3, 2, 5, 2};
+        int nums2[] = { 1, 2, 3, 2, 5, 2 };
         nums2 = Utility.notAlone(nums2, 2);
-        assertTrue(Arrays.equals(new int[] {1, 3, 3, 5, 5, 2}, nums2));
+        assertTrue(Arrays.equals(new int[] { 1, 3, 3, 5, 5, 2 }, nums2));
     }
 
     @Test
     public void notAloneTest3() {
-            int nums3[] = {3, 4};
+        int nums3[] = { 3, 4 };
         nums3 = Utility.notAlone(nums3, 3);
-        assertTrue(Arrays.equals(new int[] {3, 4}, nums3));         
+        assertTrue(Arrays.equals(new int[] { 3, 4 }, nums3));
     }
 
-            
 }
